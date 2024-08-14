43289bc9804a117338a0ef5b29131c2ff297ff52
tianathomas05 <tianathomas@utexas.edu>
2024-08-14 12:21:30 -0500

    more changes

--- app.py
+++ app.py
@@ -6,7 +6,6 @@ from datetime import datetime
 ts=time.time()
 date=datetime.fromtimestamp(ts).strftime("%d-%m-%Y")
 timestamp=datetime.fromtimestamp(ts).strftime("%H:%M-%S")
-
 df = pd.read_csv("Attendance/Attendance_" + date + ".csv")
 
 st.dataframe(df.style.highlight_max(axis=0))
